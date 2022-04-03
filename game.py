c85d085cd4475237594dbba12df91f45adc7ee72
nassimaliou <im_aliousalah@esi.dz>
2022-04-03 22:52:31 +0100

    Directions

--- game.py
+++ game.py
@@ -1,7 +1,20 @@
+from pickletools import UP_TO_NEWLINE
+from tkinter import LEFT, RIGHT
 import pygame
 import random
 
 
+from enum import Enum
+
+from zmq import DOWNSTREAM
+
 pygame.init()
 
 font = pygame.font.Font('arial.ttf', 25)
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
