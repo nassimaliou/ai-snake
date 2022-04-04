0c32c9794edb65b3048c98da1c88f56f4cf1f412
nassimaliou <im_aliousalah@esi.dz>
2022-04-04 17:27:04 +0100

    train agent function

--- agent.py
+++ agent.py
@@ -20,7 +20,7 @@ class Agent:
         self.gamma = 0 #discount rate
         self.memory = deque(maxlen=MAX_MEMORY) #popleft()
 
-        
+
 
 
 
@@ -33,7 +33,7 @@ class Agent:
     def train_long_memory(self):
         pass
 
-    def train_short_memory(self):
+    def train_short_memory(self, state, action, reward, next_state, done):
         pass
 
     def get_action(self, state):
@@ -41,7 +41,41 @@ class Agent:
 
 
 def train():
-    pass
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+
+    while True:
+        #old state
+        state_old = agent.get_state(game)
+
+        #get move
+        final_move = agent.get_action(state_old)
+
+        #move and set new state
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        #train on short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        #store memory
+
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+
+        if done:
+            #train on long memory, plot result
+            game.reset()
+            agent.number_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                #agent.model.save
 
 
 if __name__ == '__main__':
